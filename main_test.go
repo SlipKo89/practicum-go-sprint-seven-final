f9b9d407dac9ffe3343bac3660ad47bbd7f6d535
SlipKo <slipko@yandex-team.ru>
2024-05-04 11:37:14 +0300

    fix test after 1st review

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test1(t *testing.T) {
+func TestMainHandlerForCorrectResponseAndBodyIsNotEmpty(t *testing.T) {
 	//Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое.
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
 
@@ -18,17 +18,13 @@ func Test1(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	require.Equal(t, responseRecorder.Code, http.StatusOK)
-	//body_len := responseRecorder.Body.Len()
-	//assert.Greater(t, body_len, 0)
-	// Alternate variant
-	assert.NotEmpty(t, responseRecorder.Body.Len())
+
+	assert.NotEmpty(t, responseRecorder.Body)
 }
 
-func Test2(t *testing.T) {
+func TestMainHandlerForUnsupportCity(t *testing.T) {
 	//Город, который передаётся в параметре city, не поддерживается.
 	//Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
-
-	correct_reponse := "wrong city value"
 
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=Barnaul", nil)
 
@@ -36,29 +32,21 @@ func Test2(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	//fmt.Println(responseRecorder.Code)
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
-	//fmt.Println(responseRecorder.Body.String())
-	assert.Equal(t, responseRecorder.Body.String(), correct_reponse)
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+
+	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
 }
 
-func Test3(t *testing.T) {
+func TestMainHandlerForBigCount(t *testing.T) {
 	//Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе.
 
 	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=999999", nil)
-	req2 := httptest.NewRequest("GET", "/cafe?city=moscow&count=4", nil)
 
 	responseRecorder := httptest.NewRecorder()
-	responseRecorder2 := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	response_with_big_count := responseRecorder.Body.String()
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 
-	handler.ServeHTTP(responseRecorder2, req2)
-
-	response_with_true_count := responseRecorder2.Body.String()
-
-	//fmt.Println(response_with_true_count, response_with_big_count)
-	assert.Equal(t, response_with_big_count, response_with_true_count)
+	assert.Equal(t, responseRecorder.Body.String(), "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент")
 }
